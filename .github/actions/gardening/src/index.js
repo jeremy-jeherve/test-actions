24cecfd68585db61ccd610b4b6f41764e76830bc
Jeremy Herve <jeremy@jeremy.hu>
2022-07-26 12:05:44 +0200

    Change label name

--- .github/actions/gardening/src/index.js
+++ .github/actions/gardening/src/index.js
@@ -28,7 +28,7 @@ const { context, getOctokit } = require('@actions/github');
 			owner: owner.login,
 			repo: name,
 			issue_number: number,
-			labels: ['Just opened'],
+			labels: ['In Progress'],
 		});
 	}
 
