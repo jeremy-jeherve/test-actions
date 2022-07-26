2455c4be85cde15291f712526868e89ef79694d7
Jeremy Herve <jeremy@jeremy.hu>
2022-07-26 11:54:01 +0200

    Fix action

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const { setFailed, getInput, debug } = require( '@actions/core' );
-const { context, getOctokit } = require( '@actions/github' );
+const { setFailed, getInput, debug } = require('@actions/core');
+const { context, getOctokit } = require('@actions/github');
 
-( async function main() {
-	const token = getInput( 'github_token' );
-	if ( ! token ) {
-		setFailed( 'Input `github_token` is required' );
+(async function main() {
+	const token = getInput('github_token');
+	if (!token) {
+		setFailed('Input `github_token` is required');
 		return;
 	}
 
@@ -12,19 +12,18 @@ const { context, getOctokit } = require( '@actions/github' );
 	const { payload, eventName } = context;
 
 	// eslint-disable-next-line new-cap
-	const octokit = new getOctokit( token );
+	const octokit = new getOctokit(token);
 
-	debug( `Received event = '${ eventName }', action = '${ payload.action }'` );
+	debug(`Received event = '${eventName}', action = '${payload.action}'`);
 
 	// Trigger something when a Pull Request is opened.
-	if ( eventName === 'pull_request_target' && action === 'opened' ) {
-		debug( 'Pull Request opened' );
+	if (eventName === 'pull_request_target' && payload.action === 'opened') {
+		debug('Pull Request opened');
 
 		const { number, repository } = payload;
 		const { owner, name } = repository;
 
-
-		debug( `Add a label to PR #${ number }` );
+		debug(`Add a label to PR #${number}`);
 		await octokit.rest.issues.addLabels({
 			owner: owner.login,
 			repo: name,
@@ -33,5 +32,5 @@ const { context, getOctokit } = require( '@actions/github' );
 		});
 	}
 
-	debug( 'Done.' );
-} )();
+	debug('Done.');
+})();
